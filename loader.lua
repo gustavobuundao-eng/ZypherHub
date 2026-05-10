cd1f1ac57af51ad134456c0fc3ccf89b8f5d5d4e
gustavobuundao-eng <gustavobuundao@gmail.com>
2026-05-10 17:56:20 -0300

    Update URL for loading ZypherHub script

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/gustavobuundao-eng/ZypherHub/refs/heads/main/ZyferHub_PUBLIC.lua
+loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/zyferhub/main/ZypherHub_PUBLIC.lua"))()"))()
